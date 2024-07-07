37dadfda11deb4dcba492cfe8c1dc1e7c770b65b
Rakesh <rocky.cta@gmail.com>
2024-07-07 14:08:46 +0530

    Error resolved

--- MyPractices/src/com/rocky/in/WellWisher2.java
+++ MyPractices/src/com/rocky/in/WellWisher2.java
@@ -1,6 +1,6 @@
 package com.rocky.in;
 
-public class WellWisher {
+public class WellWisher2 {
 	
 	public static void main(String[] args) {
 		
@@ -13,15 +13,13 @@ public class WellWisher {
 		Greentings HindiGreetings=()-> System.out.println("Greentings in Hindi");
 		HindiGreetings.greet();
 
-		WellWisher wellWisher = new WellWisher();
+		WellWisher2 wellWisher = new WellWisher2();
 		wellWisher.wish(greentings);
 		wellWisher.wish(EnglishGreetings);
 		wellWisher.wish(HindiGreetings);
 	
 	}
 
-	
-	
 	public void wish(Greentings greet)
 	{ 
 		greet.greet(); 

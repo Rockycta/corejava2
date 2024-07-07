0e5703a8e076567e14b75d8267300e647f1d22b5
Rakesh <rocky.cta@gmail.com>
2024-07-07 22:20:06 +0530

    Added lambda

--- MyPractices/src/com/rocky2/in/SortingObjects.java
+++ MyPractices/src/com/rocky2/in/SortingObjects.java
@@ -16,13 +16,16 @@ public class SortingObjects {
 	 * 
 	 */
 		
+	Person person0 = new Person(1, "Rakesh", 35);
+	Person person1 = new Person(4, "Sreya", 29);
+	Person person2 = new Person(2, "Kanmani", 40);
+	Person person3 = new Person(3, "Chinmini",25);
 	
-	 
 	 List<Person> list = new ArrayList<Person>();
-	 list.add(new Person(1, "Rakesh", 35));
-	 list.add(new Person(4, "Sreya", 29));
-	 list.add(new Person(2, "Kanmani", 40));
-	 list.add(new Person(3, "Chinmini",25));
+	 list.add(person0);
+	 list.add(person1);
+	 list.add(person2);
+	 list.add(person3);
 	
 
 	 Collections.sort(list,new PersonComparator());
@@ -30,5 +33,14 @@ public class SortingObjects {
 	 
 	 list.stream().forEach(p->System.out.println(p.getName()));
 	 
+	 System.out.println("======Using lambda=======");
+	 
+	 Collections.sort(list,(p1,p2)->{
+		 
+		 return p1.getName().compareTo(p2.getName());
+	 });
+	 
+	 list.stream().forEach(p->System.out.println(p.getName()));
+
 	}
 }
